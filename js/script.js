8adafe61c7d6d5056f6d15e0ea1172284b9be995
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-05-15 15:53:15 -0300

    add menu drop

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function menu() {
     menu_mobile.style.display = "flex";
     menu_mobile.style.right = "0px";
     menu_mobile.style.animation = "move ease-in 400ms";
-   
+
 
     setTimeout(() => {
       document.querySelector('.menu_mobile_open').style.display = "none";
@@ -18,7 +18,7 @@ function menu() {
   document.querySelector(".menu_mobile_close").addEventListener('click', () => {
     menu_close.style.right = "-100%";
     menu_close.style.animation = "move ease-out 400ms";
-   
+
     setTimeout(() => {
       if (document.querySelector('.menu_mobile').style.right = "-100%") {
         document.querySelector('.menu_mobile').style.display = "none";
@@ -38,7 +38,7 @@ function menu() {
     element.addEventListener('click', () => {
       menu_close.style.right = "-100%";
       menu_close.style.animation = "move ease-out 400ms";
-      
+
       setTimeout(() => {
         if (document.querySelector('.menu_mobile').style.right = "-100%") {
           document.querySelector('.menu_mobile').style.display = "none";
